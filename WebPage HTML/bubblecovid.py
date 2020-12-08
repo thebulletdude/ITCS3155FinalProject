dc096fd20013f74d021238a2d1359641d9d71fe4
jwhit256 <71512964+jwhit256@users.noreply.github.com>
2020-12-07 20:52:39 -0500

    Add files via upload

--- WebPage HTML/bubblecovid.py
+++ WebPage HTML/bubblecovid.py
@@ -5,20 +5,11 @@ df = pd.read_csv('C:/Users/jpw66/OneDrive/Desktop/Visualization-Lab/Software-Eng
 # Removing empty spaces from State column to avoid errors
 df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
 
-# Creating unrecovered column
-df['Current'] = df['Total Cases'] - df['Total Deaths']
-
-# Removing China and Others from data frame
-#df = df[(df['Country'] != 'China') & (df['Country'] != 'Others')]
-
-# Creating sum of number of cases group by Country Column
-#new_df = df.groupby(['State/Territory']).agg(
-#{'Total Cases': 'sum', 'Cases in Last 7 Days': 'sum', 'Current': 'sum'}).reset_index()
 
 # Preparing data
 data = [
 go.Scatter(x=df['Cases in Last 7 Days'],
-y=df['Current'],
+y=df['Total Cases'],
 text=df['State/Territory'],
 mode='markers',
 marker=dict(size=df['Cases in Last 7 Days'] /
@@ -26,7 +17,7 @@ marker=dict(size=df['Cases in Last 7 Days'] /
 ]
 
 # Preparing layout
-layout = go.Layout(title='Corona Virus Confirmed Cases', xaxis_title="Cases in Last 7 Days", yaxis_title="Current Cases", hovermode='closest')
+layout = go.Layout(title='Corona Virus Confirmed Cases', xaxis_title="Cases in Last 7 Days", yaxis_title="Total Cases", hovermode='closest')
 
 # Plot the figure and saving in a html file
 fig = go.Figure(data=data, layout=layout)
